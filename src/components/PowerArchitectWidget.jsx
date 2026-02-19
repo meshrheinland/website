8edafc2e24c252399a4aad6753a356b0afe7c607
Sebastian Muszynski <basti@linkt.de>
2026-02-19 22:09:22 +0100

    Allow clipboard write

--- src/components/PowerArchitectWidget.jsx
+++ src/components/PowerArchitectWidget.jsx
@@ -14,6 +14,7 @@ export default function PowerArchitectWidget() {
       src="https://power-architect.meshrheinland.de"
       style={{ width: '1px', minWidth: '100%', border: 'none', display: 'block' }}
       scrolling="no"
+      allow="clipboard-write"
     />
   );
 }
